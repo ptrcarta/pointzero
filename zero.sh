7c1be75a9444b0dce97864ff62f6fb55645c283d
ptrcarta <ptrcarta@gmail.com>
2014-09-19 08:52:53 +0200

    Created and started writing the main script

---
+++ zero.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. configuration
+
+#compile to <article>
+for inputfile in $POSTFILES_FOLDER/*
+do
+    ./generateblock.sh $inputfile
+done
+
+#list
+for article in $ARTICLES/*
+do
+    timestamp=$(grep -m 1 "<!-- TIME" $article  |\
+        sed 's/.*TIME \([0-9]*\).*/\1/')
+    articletitle=$(grep -m 1 "<h1>" $article  |\
+        sed 's/<h1>\(.*\)</1>/\1/')
+
+    echo $timestamp $article $articletitle >> bloglist
+done
+
+#sort
+sort -n bloglist >sortedlist
+
+#generate index
+
+#generate pages
+
+#generate individual articles
