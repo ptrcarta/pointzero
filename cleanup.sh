bbd95a94568e294889c6d8cee43176ae247f9e2e
ptrcarta <ptrcarta@gmail.com>
2014-09-22 23:54:43 +0200

    Fixed the generatepages.sh mess, improved cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,9 +2,12 @@
 
 . configuration
 
-if [[ -n "$ARTICLES" ]] && [[ -n "$ARTICLES_PAGES" ]]
+if [[ -n "$ARTICLES" ]] && [[ -n "$ARTICLES_PAGES" ]] \
+    && [[ -n "BLOG_FOLDER" ]]
 then
-    echo $ARTICLES $ARTICLES_PAGES
+    echo $ARTICLES $ARTICLES_PAGES $BLOG_FOLDER
+    rm $BLOG_FOLDER/blogpage*
+    rm $BLOG_FOLDER/index.html
     rm $ARTICLES/*
     rm $ARTICLES_PAGES/*
 fi
--- generatepages.sh
+++ generatepages.sh
@@ -2,8 +2,8 @@ echo "INITIATE generatepages.sh" >&2
 
 POSTSXPAGE=3
 
-articles=$(wc -l sortedlist)
-pages=$((articles/POSTSXPAGE))
+articles=$(wc -l sortedlist | awk '{print $1}')
+pages=$(($articles/$POSTSXPAGE))
 
 count=1
 
@@ -11,45 +11,48 @@ while read line
 do
 
 #########################################################
-#  if [ $count % $POSTSXPAGE = 1 ] occurs only when
+#  if [[ $(($count % $POSTSXPAGE )) = 1 ]] occurs only when
 #  we are at the first post of each page
 #########################################################
-    if [ $count % $POSTSXPAGE = 1 ]
+    if [[ $(($count % $POSTSXPAGE)) == 1 ]]
     then
-        file="blogpage((count/POSTSXPAGE)).html"
-        cat header.html >$BLOG_FOLDER/$file
+        file="blogpage$((count/ POSTSXPAGE)).html"
+        cat templates/header.html >$BLOG_FOLDER/$file
     fi
 
 ## This is occurs for every post
     cat `echo $line | awk '{print $2}'` >>$BLOG_FOLDER/$file
 
+
 ## This happens after every last post in each page
-    if [ $count % $POSTSXPAGE = 0 ]
+if [[ $(( $count % $POSTSXPAGE )) == 0 ]]
     then
         pagescounter=0
+
         cat >>$BLOG_FOLDER/$file <<EOF
     <footer>
     <ul>
 EOF
-        while [[ $pagescounter -le $pages ]]
+
+        while [[ $pagescounter -lt $pages ]]
         do
-            if [[ $((count/POSTSXPAGE)) = $pagescounter ]]
+            if [[ $((($count-1)/$POSTSXPAGE)) = $pagescounter ]]
             then 
                 echo "<li class=active>" >>$BLOG_FOLDER/$file
             else
                 echo "<li>"  >>$BLOG_FOLDER/$file
             fi
-            echo "<a href=$BLOG_FOLDER/blogpage${pagescounter}.html
-            $pagescounter </a>"  >>$BLOG_FOLDER/$file
+            echo "<a href=blogpage${pagescounter}.html> $pagescounter </a>"  >>$BLOG_FOLDER/$file
             echo "</li>" >>$BLOG_FOLDER/$file
-            ((pagecounter++))
+
+            ((pagescounter++))
         done
         cat >>$BLOG_FOLDER/$file <<EOF
     </ul>
     </footer>
 EOF
 
-        cat footer.html >>$BLOG_FOLDER/$file
+        cat templates/footer.html >>$BLOG_FOLDER/$file
     fi
 
     ((count++))
